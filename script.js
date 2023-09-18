a9fe416c0ae312aa30128f06853af3ea47830cbc
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-18 23:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const main=document.body;
+const div=document.createElement("div");
+body.appendChild(div);
+				const name=document.navigator.appName;
+const version=document.navigator.appVersion;
+
+div.id="browser-info";
+const string="You are using" + name+"version"+version
+div.innerText= string;
+
+
