cd4abbd5d4c28613c723034c7f6ad785292c931f
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-19 09:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const main=document.body;
 const div=document.createElement("div");
 main.appendChild(div);
-const name=document.navigator.appName;
-const version=document.navigator.appVersion;
+const name=navigator.appName;
+const version=navigator.appVersion;
 
 div.id="browser-info";
-const string="You are using" + name+"version"+version;
+const string="You are using" + " " +name+"version"+" "+version;
 div.innerText= string;
 
 
